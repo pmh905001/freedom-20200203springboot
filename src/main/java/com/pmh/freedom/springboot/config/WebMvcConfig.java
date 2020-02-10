734a91874722ae6960933c16887bc3f42ed150bf
pmh905001 <pmh905001@126.com>
2020-02-10 18:06:08 +0800

    support session interceptor

---
+++ src/main/java/com/pmh/freedom/springboot/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.pmh.freedom.springboot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.pmh.freedom.springboot.interceptor.SessionInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private SessionInterceptor sessionInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(sessionInterceptor)
+				.addPathPatterns("/**")
+				.excludePathPatterns("/user/findAllUser2", "/user/register",
+						"/user/login", "/error");
+	}
+
+}
---
+++ src/main/java/com/pmh/freedom/springboot/interceptor/SessionInterceptor.java
@@ -0,0 +1,39 @@
+package com.pmh.freedom.springboot.interceptor;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component("sessionInterceptor")
+public class SessionInterceptor extends HandlerInterceptorAdapter {
+	
+	@Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("SessionInterceptor preHandle");
+        HttpSession session = request.getSession(false);
+        if (session != null && session.getAttribute("user") != null) {
+            return true;
+        } else {
+            PrintWriter printWriter = response.getWriter();
+            printWriter.write("{code: 501, message:\"not login!\"}");
+            return false;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("SessionInterceptor postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("SessionInterceptor afterCompletion");
+    }
+
+}
