22546e60c0559d915df6db3556bf08481e62d69e
pmh905001 <pmh905001@126.com>
2020-02-10 11:26:26 +0800

    move to package: interceptor

---
+++ src/main/java/com/pmh/freedom/springboot/interceptor/PageInterceptor2.java
@@ -0,0 +1,46 @@
+package com.pmh.freedom.springboot.interceptor;
+
+import java.util.Properties;
+
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+@SuppressWarnings({ "rawtypes", "unchecked" })
+@Intercepts({
+		@Signature(type = Executor.class, method = "query", args = {
+				MappedStatement.class, Object.class, RowBounds.class,
+				ResultHandler.class }),
+		@Signature(type = Executor.class, method = "query", args = {
+				MappedStatement.class, Object.class, RowBounds.class,
+				ResultHandler.class, CacheKey.class, BoundSql.class }), })
+public class PageInterceptor2 implements Interceptor {
+
+	@Override
+	public Object intercept(Invocation invocation) throws Throwable {
+		System.err.println("before " + invocation.getMethod().getName());
+//		Object result = invocation.getMethod().invoke(invocation.getTarget(),
+//				invocation.getArgs());
+		Object result = invocation.proceed();
+		System.err.println("after " + invocation.getMethod().getName());
+		return result;
+	}
+
+	@Override
+	public Object plugin(Object target) {
+		return Plugin.wrap(target, this);
+	}
+
+	@Override
+	public void setProperties(Properties properties) {
+	}
+
+}
